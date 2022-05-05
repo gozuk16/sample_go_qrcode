aded5faf3c147c00efed7654f8674a590a13afff
GOZU Kenichiro <gozuk16@gmail.com>
2022-05-05 11:45:57 +0900

    PNG出力にも対応

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"image/color"
+	"image/png"
 	"os"
 
 	"github.com/boombuler/barcode"
@@ -11,22 +12,36 @@ import (
 )
 
 func main() {
+	var f = flag.String("f", "", "output filename")
 	flag.Parse()
 	args := flag.Args()
-	if len(args) != 1 {
+	if len(args) == 1 && flag.NFlag() == 0 {
+		msg := args[0]
+		qrCode, _ := qr.Encode(msg, qr.M, qr.Auto)
+		printConsole(qrCode)
+	} else if len(args) == 1 && flag.NFlag() > 0 {
+		msg := args[0]
+		qrCode, _ := qr.Encode(msg, qr.M, qr.Auto)
+		outputFile(qrCode, *f)
+	} else {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	msg := args[0]
-	qrCode, _ := qr.Encode(msg, qr.M, qr.Auto)
-	//qrCode, _ = barcode.Scale(qrCode, 30, 30)
-	black := "\033[40m  \033[0m"
-	white := "\033[47m  \033[0m"
-	printQrCode(qrCode, black, white)
 }
 
-func printQrCode(qrCode barcode.Barcode, black, white string) {
+func outputFile(qrCode barcode.Barcode, filename string) {
+	//qrCode, _ = barcode.Scale(qrCode, 30, 30)
+	file, _ := os.Create(filename)
+	defer file.Close()
+	png.Encode(file, qrCode)
+	fmt.Println("output:", filename)
+}
+
+func printConsole(qrCode barcode.Barcode) {
+	const black = "\033[40m  \033[0m"
+	const white = "\033[47m  \033[0m"
+
 	rect := qrCode.Bounds()
 	fmt.Println()
 	for y := rect.Min.Y; y <= rect.Max.Y+1; y++ {
